64aa2a0819a1ff5fefb947eccc606bdb4a1e8f61
mlgibson8 <michael.l.gibson@du.edu>
2023-05-25 14:58:11 -0600

    setup MVC structure, routes and sequelize

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/routes'));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+    });
