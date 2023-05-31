74a774374491834695dfaef13d85fb0d7b44f30a
Kali Lott <kalilott@Kalis-MacBook-Pro.local>
2023-05-30 21:20:11 -0600

    Fixes and changes to handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes.js');
 
 router.use('/', homeRoutes);
 module.exports = router;
