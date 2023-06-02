7529063e28380ebe7930d35362837907a91cb569
Kali Lott <kalilott@Kalis-MacBook-Pro.local>
2023-06-01 19:21:42 -0600

    Small bug fixes in .env and user-seeds.js

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,27 +2,27 @@ const { User } = require('../models');
 
 const userData = [
     {
-        name: 'John',
+        username: 'John',
         email: 'john123@gmail.com',
         password: 'password123'
     },
     {
-        name: "Lernantino",
+        username: "Lernantino",
         email: "lernantino@gmail.com",
         password: "password12345"
       },
       {
-        name: "Amiko",
+        username: "Amiko",
         email: "amiko2k20@aol.com",
         password: "password12345"
       },
       {
-        name: "Jordan",
+        username: "Jordan",
         email: "jordan99@msn.com",
         password: "password12345"
       },
       {
-        name: "Blake",
+        username: "Blake",
         email: "the_blake@yahoo.com",
         password: "password12345"
       }
