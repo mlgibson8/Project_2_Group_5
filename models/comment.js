559c367d3710b7df9caa85b0c13b2681931cc1d0
mlgibson8 <michael.l.gibson@du.edu>
2023-06-01 18:18:23 -0600

    fixed upload bugs

--- models/comment.js
+++ models/comment.js
@@ -45,37 +45,3 @@ Comment.init(
 
 module.exports = Comment;
 
-const { Model, DataTypes} = require('sequelize');
-const sequelize = require('../config/connection');
-
-class Comment extends Model {}
-
-
-Comment.init(
-    {
-            id: { 
-            type: DataTypes.INTEGER,
-            allowNull: false,
-         //   primaryKey: true,
-            autoIncrement: true,
-    },
-    text: { 
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-   // user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-},
-   // },
-{
-    sequelize,
-    timestamp: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'comment',
-},
- 
-);
-
-module.exports = {Comment};
