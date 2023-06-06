6ec66e101bc64db3df426c62c320f7939073d6ba
Kali Lott <kalilott@kalis-mbp.mynetworksettings.com>
2023-06-06 16:27:08 -0600

    Posts don't render yet on homepage but they do work from dashboard

--- public/js/view-comments.js
+++ public/js/view-comments.js
@@ -1,8 +1,8 @@
-// Add the event listener to the edit button
-const viewCommentBtns = document.querySelectorAll('.view-comments');
-// show the comments for the post
-viewCommentBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    window.location.href = `/post/${btn.dataset.postId}`;
-  });
-});
+// // Add the event listener to the edit button
+// const viewCommentBtns = document.querySelectorAll('.view-comments');
+// // show the comments for the post
+// viewCommentBtns.forEach((btn) => {
+//   btn.addEventListener('click', () => {
+//     window.location.href = `/post/${btn.dataset.postId}`;
+//   });
+// });
