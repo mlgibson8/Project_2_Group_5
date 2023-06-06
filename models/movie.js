adf0b7d9064c2a687253e0ca1319a774339c86dc
mlgibson8 <michael.l.gibson@du.edu>
2023-06-05 18:58:59 -0600

    fixed movie models

--- models/movie.js
+++ models/movie.js
@@ -1,6 +1,45 @@
-const { Model, datatype } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Movie extends Model { }
+class Movie extends Model {}
 
-module.exports = { Movie };
+Movie.init(
+    {
+     id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+     post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'post',
+            key: 'id',
+        },
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'movie',
+}
+);
+module.exports = Movie;
